880088559004d0460b84b485f44f8901b2a30811
Meher Yar Khan <meheryarkhan90@hotmail.com>
2023-10-10 19:01:10 +0300

    Updated Searching Icons and animation

--- Program.cs
+++ Program.cs
@@ -439,7 +439,7 @@ do
             string dogDescription = "";
             
             // Update to "rotating" animation with countdown
-            string[] searchingIcons = {".  ", ".. ", "..."};
+            string[] searchingIcons = {"--", "\\ ", "| ", "/ "};
 
             // Loop ourAnimals array to search for matching animals
             for (int i = 0; i < maxPets; i++)
@@ -450,12 +450,12 @@ do
                     dogDescription = ourAnimals[i, 4] + "\r\n" + ourAnimals[i, 5];
                     
                     // Searching Animation loop
-                    for (int j = 5; j > -1 ; j--)
+                    for (int j = 2; j > -1 ; j--)
                     {
                     // Update "searching" message to show countdown 
                         foreach (string icon in searchingIcons)
                         {
-                            Console.Write($"\rsearching our dog {ourAnimals[i, 3]} for {dogCharacteristic} {icon}");
+                            Console.Write($"\rsearching our dog {ourAnimals[i, 3]} for {dogCharacteristic} {icon} {j}");
                             Thread.Sleep(250);
                         }
                         
